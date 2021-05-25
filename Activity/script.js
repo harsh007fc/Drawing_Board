b061da04bcd915c7d116f282212d305f89ebe991
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-25 10:33:40 +0530

    code added updo line drawn

--- Activity/script.js
+++ Activity/script.js
@@ -13,5 +13,49 @@ bars.addEventListener("click",function(){
 
 board.height = window.innerHeight;
 board.width = window.innerWidth;
+let tool = board.getContext("2d");
+window.addEventListener("resize",function(){
+    board.height = window.innerHeight;
+    board.width = window.innerWidth;
+    draw();
+})
+draw();
+function draw(){
+    tool.fillStyle = "#333";
+    tool.fillRect(0,0,window.innerWidth,window.innerHeight);
+
+
+    let isMouseDown = false;
+    board.addEventListener("mousedown",function(e){
+        let x = e.clientX;
+        let y = e.clientY;
+        tool.beginPath();
+        tool.moveTo(x,y);
+        isMouseDown = true;
+
+    });
+    board.addEventListener("mousemove",function(e){
+        let x = e.clientX;
+        let y = e.clientY;
+        if(isMouseDown == true){
+            tool.lineTo(x,y);
+            tool.stroke();
+        }
+
+    });
+    board.addEventListener("mouseup",function(e){
+        let x = e.clientX;
+        let y = e.clientY;
+        tool.lineTo(x,y);
+        tool.stroke();
+        isMouseDown = false;
+
+    });
+}
+
+
+
+
+
 
 
