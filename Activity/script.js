151d36e232a45be300e094e30ea8dfc350369d62
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-25 12:38:51 +0530

    code added upto pencil done /width not adjustable/

--- Activity/script.js
+++ Activity/script.js
@@ -1,13 +1,30 @@
 let bars = document.querySelector(".bars");
 let tools = document.querySelector(".tools");
 let board = document.querySelector(".board");
+let fas = document.querySelectorAll(".fas");
+let pencil = document.querySelector(".fa-pencil-alt"); 
+let eraser = document.querySelector(".fa-eraser"); 
+let colorPicker = document.querySelector(".color-picker");
+let redColor = document.querySelector(".red");
+let greenColor = document.querySelector(".green");
+let blueColor = document.querySelector(".blue");
+let whiteColor = document.querySelector(".white");
+let yellowColor = document.querySelector(".yellow");
 
 
+//*********to change selecte tool color********//
+for(let i = 0; i < fas.length; i++){
+    fas[i].addEventListener("click",function(e){
+        for(let i = 0; i < fas.length; i++){
+            fas[i].classList.remove("tool-active");
+        }
+        e.target.classList.add("tool-active");
+    })
+}
 
 bars.addEventListener("click",function(){
     bars.classList.toggle("change");
     tools.classList.toggle("tools-inactive");
-    
 });
 
 
@@ -24,6 +41,30 @@ function draw(){
     tool.fillStyle = "#333";
     tool.fillRect(0,0,window.innerWidth,window.innerHeight);
 
+    pencil.addEventListener("click",function(){
+        tool.strokeStyle = "black";
+        tool.lineWidth = 2;
+        colorPicker.classList.toggle("hide");
+        redColor.addEventListener("click",function(){
+            tool.strokeStyle = "red";
+        })
+        greenColor.addEventListener("click",function(){
+            tool.strokeStyle = "green";
+        })
+        blueColor.addEventListener("click",function(){
+            tool.strokeStyle = "blue";
+        })
+        whiteColor.addEventListener("click",function(){
+            tool.strokeStyle = "white";
+        })
+        yellowColor.addEventListener("click",function(){
+            tool.strokeStyle = "yellow";
+        })
+    })
+    eraser.addEventListener("click",function(){
+        tool.strokeStyle = "#333";
+        tool.lineWidth = 10;
+    })
 
     let isMouseDown = false;
     board.addEventListener("mousedown",function(e){
