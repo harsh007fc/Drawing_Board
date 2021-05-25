40e8f389ab722bdc57394d3a1fddce75825d6555
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-25 08:30:42 +0530

    code added upto icons added and canvas setup

--- Activity/script.js
+++ Activity/script.js
@@ -1,11 +1,17 @@
 let bars = document.querySelector(".bars");
 let tools = document.querySelector(".tools");
+let board = document.querySelector(".board");
 
 
 
 bars.addEventListener("click",function(){
     bars.classList.toggle("change");
     tools.classList.toggle("tools-inactive");
+    
 });
+
+
+board.height = window.innerHeight;
+board.width = window.innerWidth;
 
 
