449da06e20b3c5e6b55af911e23cc744ba4f25c9
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-27 20:02:20 +0530

    code added upto adjustments

--- Activity/script.js
+++ Activity/script.js
@@ -53,87 +53,86 @@ function draw() {
         console.log("drawn");
         tool.putImageData(memory[i],0,0);
     }
-
-    /////////////////////////////////////////////////////////////////
-    let lastSelectedColor; //variable for previous seleced color of penc 
-    let selectedColor; //to track color of current selected color
-    pencil.addEventListener("click", function () {
-            colorPicker.classList.add("unhide");
-            widthBox.classList.add("unhide");
-            eraserWidthBox.classList.remove("unhide");
-            tool.lineWidth = pencilSlider.value;
-            if(selectedColor == "#333"){
-                console.log(selectedColor);
-                selectedColor = lastSelectedColor;
-                tool.strokeStyle = selectedColor;
-            }
-    });
-//************************************************************************************************************** *//
-    redColor.addEventListener("click", function () {
-        selectedColor = "#eb3b5a";
-        lastSelectedColor = "#eb3b5a";
-        tool.strokeStyle = selectedColor;
-        // tool.strokeStyle = "#eb3b5a";
-    });
-    greenColor.addEventListener("click", function () {
-        selectedColor = "#20bf6b";
-        lastSelectedColor = "#20bf6b";
-        tool.strokeStyle = selectedColor;
-        // tool.strokeStyle = "#20bf6b";
-    });
-    blueColor.addEventListener("click", function () {
-        selectedColor = "#45aaf2";
-        lastSelectedColor = "#45aaf2";
-        tool.strokeStyle = selectedColor;
-        // tool.strokeStyle = "#45aaf2";
-    });
-    blackColor.addEventListener("click", function () {
-        selectedColor = "black"; 
-        lastSelectedColor = "black"; 
-        tool.strokeStyle = selectedColor;
-        // tool.strokeStyle = "black";
-    });
-    yellowColor.addEventListener("click", function () {
-        selectedColor = "#fed330";
-        lastSelectedColor = "#fed330";
-        tool.strokeStyle = selectedColor;
-        // tool.strokeStyle = "#fed330";
-    });
-    
-    pencilSlider.addEventListener("change",function(){
-        tool.lineWidth = pencilSlider.value
-    });
-    
-    pencil.addEventListener("dblclick",function(){
-        colorPicker.classList.remove("unhide");
-        widthBox.classList.remove("unhide");
-    });
-//***************************************************************************************************************************** //
-    eraser.addEventListener("click", function () {
-            activeTool = "eraser";
-            // tool.strokeStyle = "#333";
-            selectedColor = "#333";
-            tool.strokeStyle = selectedColor;
-            eraserWidthBox.classList.add("unhide");
-            colorPicker.classList.remove("unhide");
-            widthBox.classList.remove("unhide");
-            tool.lineWidth = eraserSlider.value;
-         
-    });
-    eraserSlider.addEventListener("change",function(){
-        tool.lineWidth = eraserSlider.value
-    });
-    eraser.addEventListener("dblclick", function () {
-        eraserWidthBox.classList.remove("unhide");
-    });
-// -----------------
-
-// -----------------
-// //////////////////////////////////////////////////////
+//------------------------------------------------------------------
    
+
+    ///----------------------------------------------------------------
 }
 
+//-------------------pencil and eraser ----------------------
+let lastSelectedColor; //variable for previous seleced color of penc 
+let selectedColor; //to track color of current selected color
+pencil.addEventListener("click", function () {
+        colorPicker.classList.add("unhide");
+        widthBox.classList.add("unhide");
+        eraserWidthBox.classList.remove("unhide");
+        tool.lineWidth = pencilSlider.value;
+        if(selectedColor == "#333"){
+            console.log(selectedColor);
+            selectedColor = lastSelectedColor;
+            tool.strokeStyle = selectedColor;
+        }
+});
 
+
+redColor.addEventListener("click", function () {
+    selectedColor = "#eb3b5a";
+    lastSelectedColor = "#eb3b5a";
+    tool.strokeStyle = selectedColor;
+    // tool.strokeStyle = "#eb3b5a";
+});
+greenColor.addEventListener("click", function () {
+    selectedColor = "#20bf6b";
+    lastSelectedColor = "#20bf6b";
+    tool.strokeStyle = selectedColor;
+    // tool.strokeStyle = "#20bf6b";
+});
+blueColor.addEventListener("click", function () {
+    selectedColor = "#45aaf2";
+    lastSelectedColor = "#45aaf2";
+    tool.strokeStyle = selectedColor;
+    // tool.strokeStyle = "#45aaf2";
+});
+blackColor.addEventListener("click", function () {
+    selectedColor = "black"; 
+    lastSelectedColor = "black"; 
+    tool.strokeStyle = selectedColor;
+    // tool.strokeStyle = "black";
+});
+yellowColor.addEventListener("click", function () {
+    selectedColor = "#fed330";
+    lastSelectedColor = "#fed330";
+    tool.strokeStyle = selectedColor;
+    // tool.strokeStyle = "#fed330";
+});
+
+pencilSlider.addEventListener("change",function(){
+    tool.lineWidth = pencilSlider.value
+});
+
+pencil.addEventListener("dblclick",function(){
+    colorPicker.classList.remove("unhide");
+    widthBox.classList.remove("unhide");
+});
+
+eraser.addEventListener("click", function () {
+        activeTool = "eraser";
+        // tool.strokeStyle = "#333";
+        selectedColor = "#333";
+        tool.strokeStyle = selectedColor;
+        eraserWidthBox.classList.add("unhide");
+        colorPicker.classList.remove("unhide");
+        widthBox.classList.remove("unhide");
+        tool.lineWidth = eraserSlider.value;
+     
+});
+eraserSlider.addEventListener("change",function(){
+    tool.lineWidth = eraserSlider.value
+});
+eraser.addEventListener("dblclick", function () {
+    eraserWidthBox.classList.remove("unhide");
+});   
+//-------------------pencil and eraser ----------------------
 
 //----------------------------------PENCIL STROKE---------------//
 let isMouseDown = false;
